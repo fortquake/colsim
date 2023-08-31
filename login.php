2d0fd935d8bf5cb26170f38a82dc4dbba46f2261
Fort Quake <48767278+fortquake@users.noreply.github.com>
2023-08-31 20:52:32 +0100

    wiki adding

---
+++ login.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>Colcraft Dating Simulator</title>
+    </head>
+
+    <body>
+        <div class="header">
+            <h1>Colcraft Dating Simulator</h1>
+            <p>Please log in with your discord username and the default password you received.  You can change this later.  If you did not receive a password, please request one from @fortquake on discord.</p>
+
+            <p>
+                <a href="wiki.html">How to play!</a>
+            </p>
+        </div>
+
+        <div class="login">
+            <form action="login.php" method="post">
+                <input type="text" name="username" placeholder="Username" required="required" />
+                <input type="password" name="password" placeholder="Password" required="required" />
+                <button type="submit" class="btn btn-primary btn-block btn-large">Log in</button>
+            </form>
+
+        </div>
+
+        <div style="position: absolute; bottom: 0px; text-align: center; width: 100%; left: 0px; background-color: grey" class="footer">
+            <span absolute>
+                <p>
+                    Colcraft Dating Simulator (2023)
+                </p>
+                <p>
+                    A copy of the legal notice can be found at <a href="legal.html">legal.html</a>
+                </p>
+            </span>
+        </div>
+
+    </body>
+</html> 
